82ad8a5dc701df380c5d0dcf61aea7e55f350d13
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-19 22:08:41 +0700

    Fix bug

--- app/handler/index.js
+++ app/handler/index.js
@@ -2,7 +2,7 @@ const { MESSAGE_SOURCE_TYPE } = require('../data/messagingAPI/messageSourceType'
 const groupHandler = require('./groupHandler')
 
 module.exports = (event) => {
-  const source = event.message.source;
+  const source = event.source.type;
   switch (source) {
     case MESSAGE_SOURCE_TYPE.GROUP:
       return groupHandler(event);
