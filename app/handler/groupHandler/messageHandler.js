7321621ea9ed7d83cb7b6e3f61614e9c4b222ff6
Fernando Christyanto <christyantofernando@gmail.com>
2018-09-20 00:11:25 +0700

    Add tutorial

--- app/handler/groupHandler/messageHandler.js
+++ app/handler/groupHandler/messageHandler.js
@@ -20,7 +20,41 @@ module.exports = async (event) => {
       return await handleLeave(event)
     case COMMAND.START:
       break;
+    case COMMAND.LIST:
+      return await handleList(event)
+    case COMMAND.TUTORIAL:
+      return handleTutorial(event)
   }
+}
+
+function handleTutorial() {
+  const text = `
+  UndercoverBOT adalah bot yang membantu user-user di sebuah group dalam bermain undercover.\n\n
+  Apa itu undercover?\n
+  Undercover adalah permainan dimana rolenya terdiri dari : whiteguy, undercover1, undercover2, dan sisanya member.\nSetiap user akan diacak dan dibagikan role. Selain role, setiap user juga akan dibagikan kalimat.\Whiteguy, undercover1 dan undercover2 juga akan diberikan kalimat, akan tetapi bukanlah kalimat yang sama dengan kalimat yang diberikan pada para member, melainkan kalimat yang menyerupai kalimat yang diberikan ke para member.\n\nSetiap putarannya, semua user akan secara bergantian mendeskripsikan kalimat yang didapat.\n\nGoalnya untuk para member adalah mengidentifikasi para undercover dan whiteguy.\n\nGoal dari undercover adalah tetap mendeskripsikan kalimatnya sedemikian mungkin agar tidak ketahuan oleh para member bahwa dirinya adalah undercover.\n\nGoal dari whiteguy adalah menebak kalimat yang diberikan ke para member.\nPada akhir tiap putarannya, seluruh user akan diminta untuk vote salah 1 user. User dengan vote terbanyak akan di eliminasi dari permainan\n\n
+  Kondisi Menang: \n
+  - Member menang apabila, berhasil mengeliminasi seluruh undercover dan whiteguy\n
+  - Undercover menang apabila setiap member dan whiteguy telah ter eliminasi\n
+  - Whiteguy menang apabila berhasil menebak kalimat yang dimiliki oleh member
+
+  \n\n\n
+  List perintah undercoverBOT:\n
+  - join\n
+  Bergabung ke permainan\n\n
+  - leave\n
+  Keluar dari permainan\n\n
+  - list\n
+  Akan menampilkan seluruh user yang telah join\n\n
+  - start\n
+  Mulai permainan\n\n
+  - tutorial\n
+  Menampilkan tutorial permainan
+  `
+
+  return client.replyMessage(event.replyToken, {
+    type: MESSAGE_TYPE.TEXT,
+    text: text
+  })
 }
 
 async function handleList(event) {
