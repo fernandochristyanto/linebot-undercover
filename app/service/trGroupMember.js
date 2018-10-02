146baf9f9a08b15039cb3791be2f86c441ad810a
Fernando Christyanto <christyantofernando@gmail.com>
2018-10-02 17:58:03 +0700

    Fix bug

--- app/service/trGroupMember.js
+++ app/service/trGroupMember.js
@@ -1,9 +1,15 @@
 const db = require('../model')
+const ObjectId = require('mongodb').ObjectId;
 
 exports.getUnEliminatedMembers = async (groupId) => {
   return await db.TrGroupMember.find({ groupId: groupId, $or: [{ eliminated: false }, { eliminated: undefined }] })
 }
 
 exports.clearGroupMemberVote = async (groupId) => {
-  return await db.TrGroupMember.find({ groupId: groupId }).update({ voted: false, voteUserId: undefined })
+  return await db.TrGroupMember.update({
+    groupId: new ObjectId(groupId)
+  }, {
+    voted: false, voteUserId: undefined
+  })
+  // return await db.TrGroupMember.find({ groupId: groupId }).update({ voted: false, voteUserId: undefined })
 }
